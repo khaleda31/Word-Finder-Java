fa0f2505b2f389caa498f63e08e7002b0c81a0ea
Nur Khaleda Mohd Hilmi <nurkhaleda.mh@gmail.com>
2024-07-23 17:17:03 +0800

    Create IPosition.java

---
+++ IPosition.java
@@ -0,0 +1,11 @@
+package F28PB_CW;
+
+/*In-file position*/
+public interface IPosition {
+
+	/* Returns the file name of the position. */
+	public String getFileName();
+
+	/* Returns the line number of the position. */
+	public int getLineNumber();
+}
